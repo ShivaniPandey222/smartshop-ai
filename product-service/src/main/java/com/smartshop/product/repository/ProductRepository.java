59329c142a7176f4deb694481e9829ac191fc05c
ShivaniPandey222 <pandeyshivani6777@gmail.com>
2026-06-11 15:41:04 +0530

    Enhance product service with additional CRUD operations and exception handling

--- product-service/src/main/java/com/smartshop/product/repository/ProductRepository.java
+++ product-service/src/main/java/com/smartshop/product/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.smartshop.product.repository;
 
 import com.smartshop.product.model.Product;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-
+     List<Product> findByNameLikeIgnoreCase(String name);
 }
